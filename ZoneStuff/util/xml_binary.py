a464170f053faded0726414e9300126ec869adb8
Rhett <reticentcool@protonmail.com>
2019-09-17 00:10:51 -0400

    Uses value instead of x in the xml now

--- ZoneStuff/util/xml_binary.py
+++ ZoneStuff/util/xml_binary.py
@@ -5,20 +5,20 @@ def element_section(parent: Element, name: str) -> Element:
     return SubElement(parent, 'section', name=name)
 
 
-def element_int(parent: Element, x: int, name: str) -> Element:
-    return SubElement(parent, 'int', x=str(x), name=name)
+def element_int(parent: Element, value: int, name: str) -> Element:
+    return SubElement(parent, 'int', value=str(value), name=name)
 
 
-def element_float(parent: Element, x: float, name: str) -> Element:
-    return SubElement(parent, 'float', x=str(x), name=name)
+def element_float(parent: Element, value: float, name: str) -> Element:
+    return SubElement(parent, 'float', value=str(value), name=name)
 
 
-def element_string(parent: Element, x: str, name: str) -> Element:
-    return SubElement(parent, 'string', x=x, name=name)
+def element_string(parent: Element, value: str, name: str) -> Element:
+    return SubElement(parent, 'string', value=value, name=name)
 
 
-def element_bytes(parent: Element, x: bytes, name: str) -> Element:
-    return SubElement(parent, 'bytes', length=str(len(x)), x=''.join([f'{y:02x}' for y in x]), name=name)
+def element_bytes(parent: Element, value: bytes, name: str) -> Element:
+    return SubElement(parent, 'bytes', length=str(len(value)), value=''.join([f'{y:02x}' for y in value]), name=name)
 
 # def element_pair(parent: Element, pair, name: str) -> Element:
 #     print(pair)
